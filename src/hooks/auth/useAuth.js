400068335e40f3e8daa40dc47561c5eada7fe12d
as-group <asadbek.sheraliyev07@mail.ru>
2022-07-04 19:05:55 +0500

    fix

--- src/hooks/auth/useAuth.js
+++ src/hooks/auth/useAuth.js
@@ -14,11 +14,12 @@ export async function useAuth (obj){
     const response = (await axios.post(`/v1/auth/login`, obj)).data;
     localStorage.setItem('access_token',response.access_token);
     localStorage.setItem('refresh_token',response.refresh_token);
+    axios.defaults.headers.common['Authorization'] = 'Bearer ' + localStorage.getItem('access_token');
     console.log(router)
     router.push('/')
   } catch (e) {
     toast('400','error')
-    console.log(e)
+    console.log('res',e)
   }
 }
 
