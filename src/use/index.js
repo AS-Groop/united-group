a212908d0689f31b5c3a721957f23218a42a3fbe
AS-Groop <asadbek.sheraliyev07@mail.ru>
2022-07-25 08:14:07 +0500

    fix

--- src/use/index.js
+++ src/use/index.js
@@ -42,7 +42,7 @@ axios.interceptors.response.use(config => {
     return config
 }, async error => {
     const originalRequest = error.config;
-    if (error.response.data.error_code === 400 && error.response.data.error_message === 'error occured while authorization'){
+    if (error.response.data.error_code === 400 && error.response.data.error_message === 'error occured while authorization' || error.response.data.error_code===500){
         originalRequest._isRetry = true;
         if(localStorage.getItem('refresh_token')){
             try{
