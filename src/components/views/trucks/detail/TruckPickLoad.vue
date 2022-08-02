919ac3075fd4140192a5e669943fef68e5c60bf4
as-group <asadbek.sheraliyev07@mail.ru>
2022-08-02 17:22:05 +0500

    fixed

--- src/components/views/trucks/detail/TruckPickLoad.vue
+++ src/components/views/trucks/detail/TruckPickLoad.vue
@@ -1,13 +1,14 @@
 <template>
   <label class="trucks__pick-inputs">
-    <img v-if="img" src="#" alt="">
-    <v-svg v-else :id="svg" width="106" height="106"/>
+    <span class="wrapper"><b class="text-h0">+{{ count || 0 }}</b> <span class="text-h2-med"> {{ title }} </span></span>
+    <img v-if="img" src="@/assets/images/tires1.png" alt="">
+    <v-svg :id="svg" width="106" height="106"/>
   </label>
 </template>
 <script>
 import VSvg from "@/components/ui/vSvg";
 export default {
   components: {VSvg},
-  props:['img','svg']
+  props:['img','svg','title','count']
 }
 </script>
--- src/components/views/trucks/detail/TrucksPick.vue
+++ src/components/views/trucks/detail/TrucksPick.vue
@@ -8,17 +8,17 @@
       </div>
       <div class="trucks__pick-loads">
         <p class="text-h4">Please upload an image of the odometer and fuel level to help verify the mileage and fuel used during your trip!</p>
-        <TruckPickLoad @click="$emit('loadPhoto',{name:'Odometer image'})" svg="odometer" :img="img" />
-        <TruckPickLoad @click="$emit('loadPhoto',{name:'Fuel level image'})" svg="full-level" :img="img" />
+        <TruckPickLoad @click="$emit('loadPhoto',{name:'Odometer image'})" title="Odometer image" svg="odometer" :img="img" />
+        <TruckPickLoad @click="$emit('loadPhoto',{name:'Fuel level image'})" title="Fuel level image" svg="full-level" :img="img" />
       </div>
       <div class="trucks__pick-loads">
         <p class="text-h4">Please upload a minimum of 5 images of the following!</p>
-        <TruckPickLoad @click="$emit('loadPhoto',{name:'Drive side of the vehicle'})" svg="track-side" :img="img" />
-        <TruckPickLoad @click="$emit('loadPhoto',{name:'Front of the vehicle'})" svg="truck-front" :img="img" />
-        <TruckPickLoad @click="$emit('loadPhoto',{name:'Passenger side'})" svg="truck-side2" :img="img" />
-        <TruckPickLoad @click="$emit('loadPhoto',{name:'Back of the vehicle'})" svg="truck-back" :img="img" />
-        <TruckPickLoad @click="$emit('loadPhoto',{name:'Tires'})" svg="truck-tires" :img="img" />
-        <TruckPickLoad @click="$emit('loadPhoto',{name:'Any damages'})" svg="truck-damages" :img="img" />
+        <TruckPickLoad @click="$emit('loadPhoto',{name:'Drive side of the vehicle'})" title="Drive side of the vehicle" svg="track-side" :img="img" />
+        <TruckPickLoad @click="$emit('loadPhoto',{name:'Front of the vehicle'})" title="Front of the vehicle" svg="truck-front" :img="img" />
+        <TruckPickLoad @click="$emit('loadPhoto',{name:'Passenger side'})" title="Passenger side" svg="truck-side2" :img="img" />
+        <TruckPickLoad @click="$emit('loadPhoto',{name:'Back of the vehicle'})" title="Back of the vehicle" svg="truck-back" :img="img" />
+        <TruckPickLoad @click="$emit('loadPhoto',{name:'Tires'})" title="Tires" svg="truck-tires" :img="img" />
+        <TruckPickLoad @click="$emit('loadPhoto',{name:'Any damages'})" title="Any damages" svg="truck-damages" :img="img" />
       </div>
       <div class="trucks__pick-selects">
         <TruckPickSelect @click="$emit('modal1')" title="In cab devices" count="4"/>
@@ -37,7 +37,7 @@ export default {
   components: {TruckPickSelect, TruckPickLoad, VSvg, VInput},
   data(){
     return{
-      img:false
+      img:'https://priem.stankin.ru/upload/landing/09c/c86jma8jukreqbujathoanebut3eq8xd/banner_610x610_poday-dokymenti@1x.jpg'
     }
   }
 }
